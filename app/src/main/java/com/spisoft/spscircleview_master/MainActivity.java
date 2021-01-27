86324bf803595324d89768981621c915036ee5c8
SPSpisoft <spring_software@yahoo.com>
2021-01-28 02:21:36 +0330

    Initial commit

---
+++ app/src/main/java/com/spisoft/spscircleview_master/MainActivity.java
@@ -0,0 +1,23 @@
+package com.spisoft.spscircleview_master;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+
+import com.spisoft.spcircleview.CircleView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @SuppressLint("ResourceAsColor")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        CircleView CV = findViewById(R.id.cv);
+        CV.setBackgroundColor(R.color.purple_500);
+        CV.setViewSize(200);
+//
+    }
+}
